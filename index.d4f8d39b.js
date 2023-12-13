9dc9b84e3946ce95ca6deba2045d6c2bd813a721
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-12-13 17:34:45 +0100

    make build

---
+++ index.d4f8d39b.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded",function(){var t=document.getElementById("thumbs"),e=document.getElementById("largeImg");t.addEventListener("click",function(t){event.preventDefault();for(var n=t.target;n&&"A"!==n.tagName;)n=n.parentNode;if(n&&"A"===n.tagName){var a=n.getAttribute("href"),r=n.getAttribute("title");a&&(e.src=a,e.alt=r)}})});//# sourceMappingURL=index.d4f8d39b.js.map
+
+//# sourceMappingURL=index.d4f8d39b.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,20 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", ()=>{
+    const thumbsList = document.getElementById("thumbs");
+    const largeImg = document.getElementById("largeImg");
+    thumbsList.addEventListener("click", (e)=>{
+        event.preventDefault();
+        let targetElement = e.target;
+        while(targetElement && targetElement.tagName !== "A")targetElement = targetElement.parentNode;
+        if (targetElement && targetElement.tagName === "A") {
+            const newImgSrc = targetElement.getAttribute("href");
+            const newImgAlt = targetElement.getAttribute("title");
+            if (newImgSrc) {
+                largeImg.src = newImgSrc;
+                largeImg.alt = newImgAlt;
+            }
+        }
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
